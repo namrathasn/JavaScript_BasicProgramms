f9c03e19c6a24b4c258fe255a216a985cd67b9e7
Namratha S N <73111768+namrathasn@users.noreply.github.com>
2023-02-03 13:53:56 +0530

    new

---
+++ Head-Tail.js
@@ -0,0 +1,9 @@
+//Generated random number(0,1)
+let random = Math.floor(Math.random()*10) % 2;
+
+//if-else to state heads ot tails
+if(random == 1) {
+    console.log("Its Heads!")
+}else {
+    console.log("Its Tails!")
+}
