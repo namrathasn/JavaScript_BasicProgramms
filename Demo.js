20329f4788d46b2e3d176d6abff366ba37624155
Namratha S N <73111768+namrathasn@users.noreply.github.com>
2023-02-03 13:48:58 +0530

    new

---
+++ Demo.js
@@ -0,0 +1,19 @@
+console.log("hello");
+let a=10;
+console.log(a);
+let date=new Date();
+console.log(date);
+
+let fun=function(int num1 ,int num2)
+{
+    return num1+num2;
+}
+
+
+console.log(fun(10,20));
+console.log(fun);
+
+if(true)
+{
+    console.log("thi is condition statement");
+}
