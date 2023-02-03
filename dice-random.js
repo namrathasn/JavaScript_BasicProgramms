d0cab59ea35d1538f0c2ac91cd017afa4d84e4f0
Namratha S N <73111768+namrathasn@users.noreply.github.com>
2023-02-03 13:58:32 +0530

    new

---
+++ dice-random.js
@@ -0,0 +1,6 @@
+/**
+ * 2. Using Random Function to get Dice number between 
+ * 1 to 6
+ */
+ let numberOnDice = Math.floor(1 + (Math.random() * 10) % 6);
+ console.log("Number on Dice is :" + numberOnDice);
